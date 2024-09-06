86c7bf0fae6356f8aebe3945bf060ab475fa86b3
xtyns <2267396513@qq.com>
2024-09-06 22:10:39 +0800

    commit1

---
+++ Python/ClassTest/TestFile/LineFind.py
@@ -0,0 +1,21 @@
+import os
+os.system('cls')
+
+a=[[1,2],[3,4]]
+
+for i in range(2):
+    for j in range(2):
+        print(f'num={a[i][j]}')
+print(len(a))
+city_position=[
+    {
+        'name':'city01',
+        'x_pos':10,
+        'y_pos':5
+        },
+    {
+        'name':'city02',
+        'x_pos':8,
+        'y_pos':14
+    },
+]
---
+++ Python/ClassTest/TestFile/test02.py
@@ -0,0 +1,20 @@
+import os
+os.system('cls')
+
+"""程序一"""
+# num=input('输入数据源:')
+# print(sum(map(int, num)))
+"""程序二"""
+# from math import sqrt 
+# a,b,c=input('输入三角形第一条边长度:'),input('输入三角形第二条边长度:'),input('输入三角形第三条边长度:')
+# a,b,c=int(a),int(b),int(c)
+# p=(a+b+c)/2
+# area=sqrt(p*(p-a)*(p-b)*(p-c))
+# print(f'三角形面积为{area}')
+"""程序三"""
+# sum_num=0
+# i=1
+# while i<=100:
+#     sum_num+=i
+#     i+=1
+# print(f"结果为{sum_num}")
